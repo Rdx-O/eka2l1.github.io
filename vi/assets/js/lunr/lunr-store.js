4478b2cd77178b849df510e7469b48129b6eb8dc
pent0 <pent0@users.noreply.github.com>
2021-01-19 16:13:44 +0000

    jekyll build from Action 0b2a6878426d0f99109b474fbc7ebd3ea03fd549

---
+++ vi/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Website mới!",
+        "excerpt":"Đây là website mới. Đợi các thông báo mới đây nhé ngất mất. Cảm ơn.  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://12z1.com/vi/jekyll/update/2021/01/19/trang-m%E1%BB%9Bi.html",
+        "teaser": null
+      }]
---
+++ vi/build/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "New website!",
+        "excerpt":"This is our website now. You can hear new announcements here!  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://12z1.com/jekyll/update/2021/01/19/new-website.html",
+        "teaser": null
+      }]
